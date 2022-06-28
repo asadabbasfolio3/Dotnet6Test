2f9eae7c20ffa37e80591a930fe9e38470199bf1
FOLIO3\asadabbas <asadabbas@folio3.com>
2022-06-28 11:42:37 +0500

    add test check in buildspec

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Xunit;
+
+namespace Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+        [Theory]
+        [InlineData()]
+        public void Test2()
+        {
+
+        }
+    }
+}
